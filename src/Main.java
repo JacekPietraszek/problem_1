5c30ea9e5273384f1fd013b7fa42de4ff3a5dc23
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-11-19 19:20:03 +0100

    Solves problem_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int y = 0;
+        int sum = 0;
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                list.add(i);
+            }
+        }
+        while (y < list.size()) {
+            sum += list.get(y);
+            y++;
+        }
+        System.out.println(sum);
     }
 }
